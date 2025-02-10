bdb1df596ecbaf904006a0b36096a996c773ffdd
Marcelo Augusto <82960884+Marcelo-Augusto-Silva@users.noreply.github.com>
2025-02-10 19:05:10 -0300

    versao-3

--- scripts.js
+++ scripts.js
@@ -1,15 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const fadeInElements = document.querySelectorAll(".fade-in");
-    const slideInElements = document.querySelectorAll(".slide-in");
+    const fadeInElements = document.querySelectorAll(".project");
 
     function fadeInOnScroll() {
         fadeInElements.forEach((el) => {
-            if (el.getBoundingClientRect().top < window.innerHeight * 0.9) {
-                el.classList.add("visible");
-            }
-        });
-
-        slideInElements.forEach((el) => {
             if (el.getBoundingClientRect().top < window.innerHeight * 0.9) {
                 el.classList.add("visible");
             }
